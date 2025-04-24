e36e2b4b3bda8d478e8f65478293c6fd3d16dd4d
Yaloys <yanakyrpushko@gmail.com>
2025-04-25 02:13:01 +0300

    filled tables with data

---
+++ src/main/resources/db/migration/V2__inserting_product_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO product (name, weight, metalType, stoneType, price, manufacturer, size) VALUES
+('Silver stud earrings with pink cubic zirconium', 3, 'Silver', 'Pink cub. zirconium', 2146, 'Ukraine', 0),
+('Silver necklace with cubic zirconia', 6, 'Silver', 'Cub. zirconium', 8038, 'Ukraine', 47),
+('White gold necklace with cubic zirconia', 4, 'Gold', 'Pink and white cub. zirconium', 12020, 'Ukraine', 47),
+('Silver pendant with pink and white cubic zirconium', 1, 'Silver', 'Pink and white cub. zirconium', 16676, 'Ukraine', 0),
+('Silver stud earrings with cubic zirconium', 2, 'Silver', 'Cub. zirconium', 3461, 'Ukraine', 0),
+('Silver ring with cubic zirconia', 5, 'Silver', 'Cub. zirconium', 8395, 'Ukraine', 16),
+('Silver pendant with cubic zirconium', 1, 'Silver', 'Cub. zirconium', 4072, 'Ukraine', 0),
+('White gold bracelet with cubic zirconia', 4, 'Gold', 'Cub. zirconium', 28000, 'Ukraine', 170),
+('Silver ring', 2, 'Silver', 'None', 4303, 'Ukraine', 16),
+('White gold ring with sapphire and diamonds', 2, 'Gold', 'Diamond, sapphire', 58570, 'Ukraine', 17);
---
+++ src/main/resources/db/migration/V4__inserting_employee_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee (fullName, birthDate, phoneNumber, schedule, position) VALUES
+('Vyshnevska I.P.', '1986-07-16', '0995647231', 'weekdays', 'Store Owner'),
+('Levytskyi A.V.', '1984-12-11', '0667735429', 'weekdays', 'Administrator'),
+('Gavryl K.S.', '1985-08-22', '0506723490', 'weekdays', 'Accountant'),
+('Skrypal A. P.', '1980-09-13', '0996654981', 'Monday, Tuesday, Friday, Saturday', 'Security'),
+('Voznyak G.V.', '1979-01-24', '0954311076', 'Wednesday, Thursday, Sunday', 'Security'),
+('Sklyar V.A.', '1991-02-26', '0669014463', 'Monday, Wednesday, Thursday, Saturday', 'Sales Consultant'),
+('Tkach O.D.', '1992-07-12', '0952216598', 'Monday, Tuesday, Thursday, Sunday', 'Sales Consultant'),
+('Andruhovych E.V.', '1996-05-11', '0508914269', 'Tuesday, Wednesday, Friday', 'Sales Consultant');
