944eb8d56d3b5b667fc69bd18e8593f2148c68a6
Yaloys <yanakyrpushko@gmail.com>
2025-04-24 21:23:23 +0300

    initial commit

---
+++ src/test/java/com/yaloys/jewelry_store/JewelryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaloys.jewelry_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
