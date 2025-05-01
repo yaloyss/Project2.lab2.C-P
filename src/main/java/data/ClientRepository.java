320a876a3fa357526a6b60635dea56e235d20238
Yaloys <yanakyrpushko@gmail.com>
2025-05-02 01:09:54 +0300

    added repositories for entities

---
+++ src/main/java/data/ClientRepository.java
@@ -0,0 +1,8 @@
+package data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends CrudRepository <Client, Long> {
+}
---
+++ src/main/java/data/ProductRepository.java
@@ -0,0 +1,8 @@
+package data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository <Product, Long> {
+}
